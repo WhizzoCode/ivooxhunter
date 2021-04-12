d5eabdcdd0f0a48b3b39227159d6f0be90cd637c
Sergio Martín <sergio@sergiomartin.dev>
2021-04-12 14:31:50 +0200

    Add podcasts printing

--- src/index.js
+++ src/index.js
@@ -12,3 +12,17 @@ const packageJsonUrl = new URL("../package.json", import.meta.url);
 const packageJson = JSON.parse(fs.readFileSync(packageJsonUrl));
 
 console.log(`${ packageJson.name } ${ packageJson.version }`);
+
+// Print podcasts
+
+console.log();
+console.log("Mis podcasts:");
+console.log();
+
+let i = 1;
+podcasts.forEach(podcast => {
+  console.log(`  ${ i }. ${ podcast.name }`);
+  i++;
+});
+
+console.log("  0. Todos");
