815e05b6b07f9718f36b51bbb65ebf7495f540a8
Sergio Martín <sergio@sergiomartin.dev>
2021-04-12 14:13:49 +0200

    Add configuration load

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import fs from "fs";
 
+// Load configuration
+
+const configUrl = new URL("../config.json", import.meta.url);
+const config = JSON.parse(fs.readFileSync(configUrl));
+const podcasts = config.podcasts;
+
 // Print script name
 
 const packageJsonUrl = new URL("../package.json", import.meta.url);
