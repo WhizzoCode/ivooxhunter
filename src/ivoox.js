36c7ac54124cbb09d4f4b6458fd1f6b40d3b8234
Sergio Martín <sergio@sergiomartin.dev>
2021-04-14 00:45:37 +0200

    [ivoox] Add parseIvoox() function

--- src/ivoox.js
+++ src/ivoox.js
@@ -1,3 +1,7 @@
+const reDate = /^(\d{2}):(\d{2}) - (\d{2}) de (\w{3})\. de (\d{4})$/;
+const months = ["ene", "feb", "mar", "abr", "may", "jun",
+                "jul", "ago", "sep", "oct", "nov", "dic"];
+
 function page(pageNum, url) {
   const splitUrl = url.split("_");
   if (pageNum === "next") {
@@ -6,4 +10,27 @@ function page(pageNum, url) {
     splitUrl[3] = `${ pageNum }.html`;
   }
   return splitUrl.join("_");
+}
+
+function parseIvoox(document) {
+  const parsed = [];
+  document
+    .querySelectorAll("div.modulo-type-episodio")
+    .forEach(element => {
+      const titleElement = element.querySelector(".content .title-wrapper a");
+      const title = titleElement.title;
+      const fileCode = titleElement.href.split("_")[2];
+      const url = `http://ivoox.com/listen_mn_${ fileCode }_1.mp3`;
+      const splitDate = element.querySelector(".content .action .date")
+                               .title.match(reDate);
+      const date = new Date(
+        splitDate[5],
+        months.indexOf(splitDate[4]),
+        splitDate[3],
+        splitDate[1],
+        splitDate[2]
+      );
+      parsed.push({title, url, date});
+    });
+  return parsed;
 }
