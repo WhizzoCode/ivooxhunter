cfb1e26a424cdf09a4378c25186379818db9e856
Sergio Martín <sergio@sergiomartin.dev>
2021-04-14 01:37:33 +0200

    [ivoox] Add getEpisodes() function

--- src/ivoox.js
+++ src/ivoox.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import jsdom from "jsdom";
+
 const reDate = /^(\d{2}):(\d{2}) - (\d{2}) de (\w{3})\. de (\d{4})$/;
 const months = ["ene", "feb", "mar", "abr", "may", "jun",
                 "jul", "ago", "sep", "oct", "nov", "dic"];
@@ -33,4 +36,26 @@ function parseIvoox(document) {
       parsed.push({title, url, date});
     });
   return parsed;
+}
+
+async function getEpisodes(url, date, requestWait = 2000, next = false) {
+  if (!next) url = page(1, url);
+  if (typeof date === "number") {
+    date = new Date(Date.now() - (date * 24 * 60 * 60 * 1000));
+  }
+  const episodes = [];
+
+  const response = await axios.get(url);
+  const dom = new jsdom.JSDOM(response.data);
+  const pageEpisodes = parseIvoox(dom.window.document);
+  const filteredEpisodes = pageEpisodes.filter(episode => episode.date > date);
+  
+  Array.prototype.push.apply(episodes, filteredEpisodes);
+  console.log(requestWait);
+  if (pageEpisodes.length === filteredEpisodes.length) {
+    await new Promise(resolve => setTimeout(resolve, requestWait));
+    Array.prototype.push.apply(episodes, await getEpisodes(page("next", url), date, requestWait, true));
+  }
+  
+  return episodes;
 }
